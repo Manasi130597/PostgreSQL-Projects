c23ca5585192e5f5401babe9a2cbed33d0eceef2
Manasi130597 <manasijagtap1397@gmail.com>
2025-04-28 15:56:04 +0530

    Update inset_data_world.sh

--- inset_data_world.sh
+++ inset_data_world.sh
@@ -2,11 +2,9 @@
 
 if [[ $1 == "test" ]]
 then
-  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
-  echo "Test"
+  echo PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
 else
-  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
-  echo "Demo"
+  echo PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
@@ -19,20 +17,20 @@ do
       #if not found
       if [[ -z $WINNER_ID ]] 
       then
-        $($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+        echo $($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
         #WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE team=$WINNER");
       fi
       
       OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'");
       if [[ -z $OPPONENT_ID ]] 
       then
-        $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+        echo $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
         #OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE team=$OPPONENT");
       
       fi
       WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'");
       OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'");
-      $($PSQL "INSERT INTO games(year,round,winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, 
+      echo $($PSQL "INSERT INTO games(year,round,winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, 
       $OPPONENT_GOALS)")
 
   fi
