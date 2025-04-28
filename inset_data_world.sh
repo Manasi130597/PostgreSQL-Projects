8da225862f4b90ebab21f029349a57fee0efaf48
Manasi130597 <manasijagtap1397@gmail.com>
2025-04-28 16:07:36 +0530

    Update inset_data_world.sh

--- inset_data_world.sh
+++ inset_data_world.sh
@@ -8,6 +8,7 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+echo $($PSQL "TRUNCATE teams, games;");
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
 do
   if [[ $YEAR != "year" ]]
@@ -17,21 +18,21 @@ do
       #if not found
       if [[ -z $WINNER_ID ]] 
       then
-        echo $($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+        echo $($PSQL "INSERT INTO teams(name) VALUES('$WINNER')");
         #WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE team=$WINNER");
       fi
       
       OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'");
       if [[ -z $OPPONENT_ID ]] 
       then
-        echo $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+        echo $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')");
         #OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE team=$OPPONENT");
       
       fi
       WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'");
       OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'");
       echo $($PSQL "INSERT INTO games(year,round,winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, 
-      $OPPONENT_GOALS)")
+      $OPPONENT_GOALS)");
 
   fi
 done
